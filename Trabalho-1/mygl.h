5ba3ddcd8c8d6a86637252017382d6aaf3134eaf
Swehtam <mathewsalves1@hotmail.com>
2017-09-04 20:29:05 -0300

    Update mygl.h

--- Trabalho-1/mygl.h
+++ Trabalho-1/mygl.h
@@ -39,11 +39,17 @@ void PutPixel(int x, int y, color c){
 
 //Algoritimo de Bresenham para desenhar uma linha na tela
 void DrawLine(int x0, int y0, int x1, int y1, color c0, color c1){
+	
 	int dx = x1 - x0;
 	int dy = y1 - y0;
-	bool trocou = false;
-	bool inverteu = false;
 
+	//booleano para saber se houve a inversão do dx pelo dy e vice-versa
+	bool trocou = false;
+
+	//boleno para saber se ocorreu a inversão do dy
+	bool inverteu = false;
+	
+	//condição para saber se o dy é maior do que o dx, se sim, troca os valores de dx e dy e o booleano fica true
 	if (abs(dy) > abs(dx)){
 		Troca(&x0, &x1, &y0, &y1);
 		dx = x1 - x0;
@@ -58,6 +64,8 @@ void DrawLine(int x0, int y0, int x1, int y1, color c0, color c1){
 		PutPixel(x0, y0, c0);
 	}
 	
+
+	//condição para inverter os valores de x0 pro x1 e y0 por y1, e inversão da cor do começo com a cor do final da linha, caso o dx seja negativo
 	if(dx < 0){
 		Troca(&x0, &y0, &x1, &y1);
 		dx = x1 - x0;
@@ -67,6 +75,7 @@ void DrawLine(int x0, int y0, int x1, int y1, color c0, color c1){
 		c1 = c2;
 	}
 
+	//caso o dy seja negativo inverte seu valor para o positivo e o booleano fica true
 	if(dy < 0){
 		dy = -dy;
 		inverteu = true;
@@ -85,6 +94,7 @@ void DrawLine(int x0, int y0, int x1, int y1, color c0, color c1){
 	int y = y0;
 
 	while (x < x1) {
+		//incrementa a cor de acordo com sua variação
 		c0.r += dr;
 		c0.g += dg;
 		c0.b += db;
@@ -95,14 +105,16 @@ void DrawLine(int x0, int y0, int x1, int y1, color c0, color c1){
 		} else{
 			d += incr_ne;
 			x++;
+			//caso o dy seja invertido decrementa o y
 			if(inverteu){ 
 				y--;
-			}
+			}//caso ele n seja incremeta o y
 			else {
 				y++;
-				}
+			}
 		}
-
+		
+		//se houve a troca de dx por dy e vice-versa, entao coloque o pixel da coordenada x na do y e do y na do x, caso não, entao coloque na posição normal
 		if(trocou == true){
 			PutPixel(y, x, c0);
 		} else{
@@ -119,4 +131,3 @@ void DrawTriangle(int x0, int y0, int x1, int y1, int x2, int y2, color c0, colo
 
 
 #endif // _MYGL_H_
-
