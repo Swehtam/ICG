7aecd33c71dc8818488b625150758a0d27eecfae
Swehtam <mathewsalves1@hotmail.com>
2017-09-04 23:47:59 -0300

    Update main.cpp

--- Trabalho-1/main.cpp
+++ Trabalho-1/main.cpp
@@ -7,8 +7,27 @@ void MyGlDraw(void)
 	// Chame aqui as funções do mygl.h
 	//*************************************************************************
 
-	DrawTriangle(245, 54, 231, 450, 62, 70, color(255, 0, 0, 255), color(0, 255, 0, 255), color(0, 0, 255, 255));
-
+	int i = 384;
+	float dc = 63.0;
+	int dy = 96;
+	int dx = 64;
+	
+	DrawTriangle(256, 0, color(255, 0, 0, 255), 0, i, color(0, 255, 0, 255), 512, i,
+					color(0, 0, 255, 255));
+	DrawTriangle(256, 0, color(255, 0, 0, 255), 0, i, color(0, 255, 0, 255), 128, i, 
+					color(0, 255-dc, dc, 255));
+	DrawTriangle(256, 0, color(255, 0, 0, 255), 0, i, color(0, 255, 0, 255), 256, i, 
+					color(0, 255 - (2*dc), dc*2, 255));
+	DrawTriangle(256, 0, color(255, 0, 0, 255), 0, i, color(0, 255, 0, 255), i, i, 
+					color(0, 255 - (3*dc), dc*3, 255));
+	DrawTriangle(256, 0, color(255, 0, 0, 255), dx, i-dy, color(dc, 255-dc, 0, 255), 512-dx,
+ 					i-dy, color(dc, 0, 255-dc, 255));
+	DrawTriangle(256, 0, color(255, 0, 0, 255), dx*2, i - (2*dy), 
+					color(2*dc, 255 - (2*dc), 0, 255), 512 - (2*dx), i - (2*dy),
+					color(2*dc, 0, 255 - (2*dc), 255));
+	DrawTriangle(256, 0, color(255, 0, 0, 255), dx*3, i - (3*dy),
+					color(3*dc, 255 - (3*dc), 0, 255), 512 - (3*dx), i - (3*dy),
+					color(3*dc, 0, 255 - (3*dc), 255));
 
 }
 
@@ -28,4 +47,3 @@ int main(int argc, char **argv)
 
 	return 0;
 }
-
